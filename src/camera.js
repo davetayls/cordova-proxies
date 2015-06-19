fb99388b01a1ef1ba0fd7a76fc84385f58129071
davetayls <dave@the-taylors.org>
2015-06-19 11:21:42 +0100

    added camera helpers

---
+++ src/camera.js
@@ -0,0 +1,46 @@
+define(function (require, exports, module) {///<reference path="../typings/tsd.d.ts"/>
+/**
+ * Camera Helpers
+ * https://github.com/apache/cordova-plugin-camera
+ */
+var core = require('./core');
+var nav = navigator;
+nav.getUserMedia = nav.getUserMedia || nav.webkitGetUserMedia || nav.mozGetUserMedia;
+function isAvailable() {
+    return core.isAvailable() && !!navigator.camera;
+}
+exports.isAvailable = isAvailable;
+function getPicture(success, error, options) {
+    if (isAvailable()) {
+        navigator.camera.getPicture(success, error, options);
+    }
+}
+exports.getPicture = getPicture;
+function getVideoMediaStream(width, height) {
+    var video = document.createElement('video');
+    var URL = window.URL;
+    if (nav.getUserMedia) {
+        var options = {
+            audio: true,
+            video: {
+                width: width,
+                height: height
+            }
+        };
+        nav.getUserMedia(options, function (stream) {
+            video.src = URL.createObjectURL(stream);
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+        }, function (err) {
+            console.log("The following error occured: " + err.name);
+        });
+    }
+    else {
+        console.log("getUserMedia not supported");
+    }
+    return video;
+}
+exports.getVideoMediaStream = getVideoMediaStream;
+
+});
---
+++ src/camera.ts
@@ -0,0 +1,53 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+/**
+ * Camera Helpers
+ * https://github.com/apache/cordova-plugin-camera
+ */
+
+import core = require('./core');
+
+var nav:any = navigator;
+nav.getUserMedia = nav.getUserMedia ||
+  nav.webkitGetUserMedia ||
+  nav.mozGetUserMedia;
+
+export function isAvailable():boolean {
+  return core.isAvailable() && !!navigator.camera;
+}
+
+export function getPicture(success:(url:string)=>void,
+                           error:(errString:string)=>void,
+                           options:CameraOptions):void {
+  if (isAvailable()) {
+    navigator.camera.getPicture(success, error, options)
+  }
+}
+
+export function getVideoMediaStream(width:number, height:number):HTMLVideoElement {
+  var video = document.createElement('video');
+  var URL:any = (<any>window).URL;
+  if (nav.getUserMedia) {
+    var options = {
+      audio: true,
+      video: {
+        width: width,
+        height: height
+      }
+    };
+    nav.getUserMedia(options,
+      function(stream:any) {
+        video.src = URL.createObjectURL(stream);
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+      },
+      function(err:any) {
+        console.log("The following error occured: " + err.name);
+      }
+    );
+  } else {
+    console.log("getUserMedia not supported");
+  }
+  return video;
+}
